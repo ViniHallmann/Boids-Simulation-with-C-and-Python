c7f9e182a9e73587b82fc39aff3c55c9ee8412df
vinicius.hallmann@avenudecode.com <vinicius.hallmann2015@gmail.com>
2025-07-14 15:04:57 -0300

    Globals now have number of birds defined

--- front/globals.py
+++ front/globals.py
@@ -5,11 +5,12 @@ boids_lib = None
 lib_path = None
 platform_system = platform.system()
 current_dir = os.getcwd()
+NUM_BIRDS = 10
 
 SUPPORTED_PLATFORMS = ["Windows", "Darwin", "Linux"]
 LIBRARY_EXTENSIONS = {
     "Windows": "dll",
-    "Darwin": "dylib",
+    "Darwin": "so",
     "Linux": "so"
 }
 
