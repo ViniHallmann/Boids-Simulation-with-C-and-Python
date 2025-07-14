646077f62b80527f884ded9b15dd0d799313129b
vinicius.hallmann@avenudecode.com <vinicius.hallmann2015@gmail.com>
2025-07-14 17:29:31 -0300

    Update variables to be type

--- front/globals.py
+++ front/globals.py
@@ -1,11 +1,33 @@
 import platform
 import os
 
-boids_lib = None
-lib_path = None
-platform_system = platform.system()
-current_dir = os.getcwd()
-NUM_BIRDS = 10
+boids_lib:       str = None
+lib_path:        str = None
+platform_system: str = platform.system()
+current_dir:     str = os.getcwd()
+
+VISUAL_RANGE:     float = 75.0
+PROTECTED_RANGE:  float = 8.0
+CENTERING_FACTOR: float = 0.0005
+MATCHING_FACTOR:  float = 0.05
+AVOID_FACTOR:     float = 0.05
+TURN_FACTOR:      float = 0.2
+MAX_SPEED:        float = 6.0
+MIN_SPEED:        float = 3.0
+
+SCREEN_WIDTH:           int  = 1200
+SCREEN_HEIGHT:          int = 800
+SCREEN_WIDTH_MARGIN:    int  = 200
+SCREEN_HEIGHT_MARGIN:   int = 100
+MARGIN:                 int = 175
+
+NUM_BIRDS:      int     = 1000
+BIRD_WIDTH:     int     = 1
+BIRD_RADIUS:    float   = 2.5
+
+BIRD_COLOR:         tuple = (255, 255, 255)
+BACKGROUND_COLOR:   tuple = (10, 20, 40)
+
 
 SUPPORTED_PLATFORMS = ["Windows", "Darwin", "Linux"]
 LIBRARY_EXTENSIONS = {
@@ -14,4 +36,4 @@ LIBRARY_EXTENSIONS = {
     "Linux": "so"
 }
 
-library_loaded = False
+library_loaded: bool = False
