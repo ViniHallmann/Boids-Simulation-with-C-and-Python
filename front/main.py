db052323e18804a4766f01d9ae9d0a0d1fbfa382
vinicius.hallmann@avenudecode.com <vinicius.hallmann2015@gmail.com>
2025-07-14 15:05:42 -0300

    Python can now call C boids functions based on the defined structure

--- front/main.py
+++ front/main.py
@@ -3,6 +3,68 @@ import sys
 import globals
 import configs
 
+# CLASSES PARA REPRESENTAR ESTRUTURAS C
+# JA DA PRA QUEBRAR ISSO AQUI EM OUTROS CODIGO, EU SO PRECISO TER AS ESTRUTURAS REPRESENTADAS, TALVEZ DEIXAR ISSO EM GLOBALS?
+class Position(ctypes.Structure):
+    """
+    Classe para representar a posição de um boid
+    """
+    _fields_ = [
+        ("x", ctypes.c_float),
+        ("y", ctypes.c_float),
+    ]
+    
+class Velocity(ctypes.Structure):
+    """
+    Classe para representar a velocidade de um boid
+    """
+    _fields_ = [
+        ("vx", ctypes.c_float),
+        ("vy", ctypes.c_float),
+    ]
+
+class Entity(ctypes.Structure):
+    """
+    Classe para representar um boid
+    """
+    _fields_ = [
+        ("position", Position),
+        ("velocity", Velocity),
+    ]
+
+class Boids(ctypes.Structure):
+    """
+    Classe para representar o conjunto de boids
+    """
+    pass
+
+#TEM QUE FAZER DESSE JEITO POIS BOIDS NAO RECONHECE O Entity ANTES DE SER DEFINIDO
+Boids._fields_ = [
+    ("entities", ctypes.POINTER(Entity)),
+    ("count", ctypes.c_int),
+]
+##################################################################################
+
+# CHAMADAS A FUNÇÕES DA BIBLIOTECA C
+# MINHA IDEIA EH FAZER COM QUE TODAS CHAMADAS SEJAM FEITAS AQUI PARA FILTRAR O QUE ESTA SENDO CHAMADO 
+# E SE ESTA SENDO PASSADO CORRETAMENTE OS PARAMETROS
+def call_c(nomefuncao: str = "print_message"):
+    """
+    Chama a função da biblioteca C
+    """
+    if not globals.library_loaded:  return False
+    
+    try:
+        globals.boids_lib.__getattr__(nomefuncao)()  
+        return True
+        
+    except Exception as e:
+        print(f"Erro ao chamar função da biblioteca: {e}")
+        return False
+##################################################################################
+
+# UTIL
+# CRIAR utils.py PARA FUNCOES UTILITARIAS
 def load_library():
     """
     Carrega a biblioteca C
@@ -19,29 +81,53 @@ def load_library():
     except Exception as e:
         print(f"Erro ao carregar biblioteca: {e}")
         return False
+##################################################################################
 
-def call_c_function():
+def set_functions_args():
     """
-    Chama a função da biblioteca C
+    Configura os tipos de argumentos e retorno das funções da biblioteca C
     """
-    if not globals.library_loaded:  return False
+    if not globals.library_loaded:
+        print("Biblioteca não carregada.")
+        return False
     
     try:
-        globals.boids_lib.print_message()
+        globals.boids_lib.initialize_boids.argtypes = [ctypes.c_int]
+        globals.boids_lib.initialize_boids.restype = ctypes.POINTER(Boids)
+
+        globals.boids_lib.update_boids.argtypes = [ctypes.POINTER(Boids)]
+        globals.boids_lib.update_boids.restype = None
+
+        globals.boids_lib.free_boids.argtypes = [ctypes.POINTER(Boids)]
+        globals.boids_lib.free_boids.restype = None
+        
         return True
         
     except Exception as e:
-        print(f"Erro ao chamar função da biblioteca: {e}")
+        print(f"Erro ao configurar funções: {e}")
         return False
 
 def main():
     """
     Função principal
     """
-    if not load_library(): sys.exit(1)
+    if not load_library() or not set_functions_args(): sys.exit(1)
 
-    call_c_function()
+    boids = globals.boids_lib.initialize_boids(globals.NUM_BIRDS)
+    if not boids: sys.exit(1)
 
+    boid_zero = boids.contents.entities[0]
+    print(f"Python: Posição inicial do Boid 0: ({boid_zero.position.x:.2f}, {boid_zero.position.y:.2f})")
+
+    print("\nPython: Chamando a atualização do C...")
+    globals.boids_lib.update_boids(boids)
+    globals.boids_lib.update_boids(boids)
+    
+    print(f"Python: Nova posição do Boid 0: ({boid_zero.position.x:.2f}, {boid_zero.position.y:.2f})\n")
+
+    print("Python: Pedindo para C liberar a memória...")
+    globals.boids_lib.free_boids(boids)
+    print("Python: Fim do programa.")
 
 
 if __name__ == "__main__":
