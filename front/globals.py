793dab80af0bf655143df83edccfcbe0b70a31f8
vinicius.hallmann@avenudecode.com <vinicius.hallmann2015@gmail.com>
2025-07-15 00:38:01 -0300

    att

--- front/globals.py
+++ front/globals.py
@@ -6,22 +6,22 @@ lib_path:        str = None
 platform_system: str = platform.system()
 current_dir:     str = os.getcwd()
 
-VISUAL_RANGE:     float = 75.0
-PROTECTED_RANGE:  float = 8.0
-CENTERING_FACTOR: float = 0.0005
-MATCHING_FACTOR:  float = 0.05
-AVOID_FACTOR:     float = 0.05
 TURN_FACTOR:      float = 0.2
+VISUAL_RANGE:     float = 45.0
+PROTECTED_RANGE:  float = 6.0
+CENTERING_FACTOR: float = 0.0005
+AVOID_FACTOR:     float = 0.05
+MATCHING_FACTOR:  float = 0.05
 MAX_SPEED:        float = 6.0
 MIN_SPEED:        float = 3.0
 
-SCREEN_WIDTH:           int  = 1200
-SCREEN_HEIGHT:          int = 800
+SCREEN_WIDTH:           int  = 900
+SCREEN_HEIGHT:          int = 650
 SCREEN_WIDTH_MARGIN:    int  = 200
 SCREEN_HEIGHT_MARGIN:   int = 100
 MARGIN:                 int = 175
 
-NUM_BIRDS:      int     = 1000
+NUM_BIRDS:      int     = 7000
 BIRD_WIDTH:     int     = 1
 BIRD_RADIUS:    float   = 2.5
 
