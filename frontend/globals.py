0336255845b2ac34e2181b3fc3ddea0e321954d3
vinicius.hallmann@avenudecode.com <vinicius.hallmann@external.stellantis.com>
2025-07-17 14:19:40 -0300

    Att

--- frontend/globals.py
+++ frontend/globals.py
@@ -7,13 +7,13 @@ TURN_FACTOR:        float = 0.2
 VISUAL_RANGE:       float = 45.0
 PROTECTED_RANGE:    float = 6.0
 CENTERING_FACTOR:   float = 0.0005
-AVOID_FACTOR:       float = 0.05
+AVOID_FACTOR:       float = 0.1
 MATCHING_FACTOR:    float = 0.05
 MAX_SPEED:          float = 6.0
 MIN_SPEED:          float = 3.0
-MARGIN:             int = 175
-MARGIN_LINE:        bool = True
-BIRD_WIDTH:         int = 1
+MARGIN:             int   = 150
+MARGIN_LINE:        bool  = True
+BIRD_WIDTH:         int   = 1
 BIRD_RADIUS:        float = 2.5
 BIRD_COLOR:         tuple = (255, 255, 255)
 BACKGROUND_COLOR:   tuple = (10, 20, 40)
@@ -23,11 +23,11 @@ BLUR_TRANSPARENCY_VALUE: int = 15
 
 # VARIAVEIS CRITICAS - So podem ser alteradas em tempo de execucao caso o "run" do programa seja reiniciado. 
 # Pra rodar em tempo de execucao vai precisar dar um cleanup e instanciar nova simulation()
-SCREEN_WIDTH:           int  = 900
+SCREEN_WIDTH:           int = 900
 SCREEN_HEIGHT:          int = 650
-SCREEN_WIDTH_MARGIN:    int  = 200
+SCREEN_WIDTH_MARGIN:    int = 200
 SCREEN_HEIGHT_MARGIN:   int = 100
-NUM_BIRDS:      int     = 1000
+NUM_BIRDS:              int = 100
 PLATFORM_SYSTEM: str = platform.system()
 SUPPORTED_PLATFORMS = ["Windows", "Darwin", "Linux"]
 LIBRARY_EXTENSIONS = {
