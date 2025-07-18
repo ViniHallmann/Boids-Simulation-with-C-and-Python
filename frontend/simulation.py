bd37477c1116310095db6f75379e2301ebd0967b
vinicius.hallmann@avenudecode.com <vinicius.hallmann@external.stellantis.com>
2025-07-18 11:23:39 -0300

    Simulation now have only 1 boid again

--- frontend/simulation.py
+++ frontend/simulation.py
@@ -8,20 +8,13 @@ class Simulation:
         """
         print("P: Inicializando estado da simulação...")
 
-        self.boids1 = state.boids_lib.initialize_boids(
+        self.boids = state.boids_lib.initialize_boids(
             globals.NUM_BIRDS, globals.SCREEN_WIDTH, globals.SCREEN_HEIGHT, globals.SpawnMode.SPAWN_TOP_LEFT
         )
-        self.grid1 = state.boids_lib.create_grid(
+        self.grid = state.boids_lib.create_grid(
             globals.SCREEN_WIDTH, globals.SCREEN_HEIGHT, globals.VISUAL_RANGE
         )
 
-        # self.boids2 = state.boids_lib.initialize_boids(
-        #     globals.NUM_BIRDS, globals.SCREEN_WIDTH, globals.SCREEN_HEIGHT, 1
-        # )
-        # self.grid2 = state.boids_lib.create_grid(
-        #     globals.SCREEN_WIDTH, globals.SCREEN_HEIGHT, globals.VISUAL_RANGE
-        # )
-        
         self.update_args = (
             globals.VISUAL_RANGE, globals.PROTECTED_RANGE, globals.CENTERING_FACTOR,
             globals.MATCHING_FACTOR, globals.AVOID_FACTOR, globals.TURN_FACTOR,
@@ -33,15 +26,12 @@ class Simulation:
         """
         Chama a função de atualização da biblioteca C para avançar a simulação.
         """
-        state.boids_lib.update_boids(self.boids1, self.grid1, *self.update_args)
-        # state.boids_lib.update_boids(self.boids2, self.grid2, *self.update_args)
+        state.boids_lib.update_boids(self.boids, self.grid, *self.update_args, *globals.MOUSE_POS)
 
     def cleanup(self):
         """
         Libera a memória alocada pela biblioteca C.
         """
         print("P: Limpando memória da simulação C...")
-        state.boids_lib.free_grid(self.grid1)
-        state.boids_lib.free_boids(self.boids1)
-        # state.boids_lib.free_grid(self.grid2)
-        # state.boids_lib.free_boids(self.boids2)
+        state.boids_lib.free_grid(self.grid)
+        state.boids_lib.free_boids(self.boids)
