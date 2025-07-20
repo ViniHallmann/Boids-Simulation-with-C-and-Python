d5512997c35000d1f66426478697742baf348a51
vinihallmann <viniciushallmann2015@gmail.com>
2025-07-20 20:44:32 -0300

    default globals.py DONT CHANGE

--- frontend/globals.py
+++ frontend/globals.py
@@ -3,46 +3,57 @@ from c_definitions.c_structures import SpawnMode
 
 # VARIAVEIS NAO CRITICAS - Podem ser alteradas em tempo de execucao sem reiniciar o programa.
 # Implementar uma interface para alterar essas variaveis em tempo de execucao.
-"""
-Centering factor 
-Avoid factor
-Matching factor
-Max speed
-Min speed
-Bird size
-"""
 TURN_FACTOR:        float = 0.2
-VISUAL_RANGE:       float = 100.0
+VISUAL_RANGE:       float = 45.0
 PROTECTED_RANGE:    float = 6.0
-CENTERING_FACTOR:   float = 0.0005 # Coesao
+
+# COESAO
+CENTERING_FACTOR:   float = 0.0005
+
+# SEPARACAO
 AVOID_FACTOR:       float = 0.1
+
+# ALINHAMENTO
 MATCHING_FACTOR:    float = 0.05
+
+INFINITE_SCREEN:     bool = False
+
 MAX_SPEED:          float = 6.0
 MIN_SPEED:          float = 3.0
+
+MARGIN_WIDTH:       int = 2
+MARGIN_DASH_LENGTH: int = 10
 MARGIN:             int   = 150
 MARGIN_LINE:        bool  = True
+
+DRAW_PROTECTED_RANGE:   bool = False
+DRAW_VISUAL_RANGE:      bool = False
+
+SHOW_UI_PANEL = False
+
+BIRD_SIZE: int = 3
+# WHEN BIRD IS DRAWN AS A CIRCLE
 BIRD_WIDTH:         int   = 1
 BIRD_RADIUS:        float = 2.5
+################################
 BIRD_COLOR:         tuple = (255, 255, 255)
+
 BACKGROUND_COLOR:   tuple = (10, 20, 40)
-FPS: int = 120
+FPS: int = 60
 BLUR: bool = False
 BLUR_TRANSPARENCY_VALUE: int = 15
-BIRD_SIZE: int = 3
-MOUSE_MOTION: bool = True
-MOUSE_POS: tuple = (0, 0)
-MOUSE_FEAR: bool = False
-MOUSE_ATTRACTION: bool = False 
-MOUSE_FEAR_RADIUS: int = 100
-MOUSE_ATTRACTION_RADIUS: int = 200
-DRAW_PROTECTED_RANGE: bool = False
-DRAW_VISUAL_RANGE: bool = False
-MARGIN_WIDTH: int = 2
-MARGIN_DASH_LENGTH: int = 10
-SHOW_UI_PANEL = True
-PAUSED = False  # Controls simulation pause state
-RESTART_SIMULATION = False  # Flag to request simulation restart
-RESTART_SIMULATION_WITH_BOIDS = None  # Flag to restart with specific number of boids
+
+MOUSE_POS:          tuple = (0, 0)
+MOUSE_MOTION:       bool = True
+MOUSE_FEAR:         bool = False
+MOUSE_ATTRACTION:   bool = False
+MOUSE_FEAR_RADIUS:          int = 100
+MOUSE_ATTRACTION_RADIUS:    int = 200
+
+PAUSED:                         bool = False
+RESTART_SIMULATION:             bool = False 
+RESTART_SIMULATION_WITH_BOIDS:  bool = False
+
 
 # VARIAVEIS CRITICAS - So podem ser alteradas em tempo de execucao caso o "run" do programa seja reiniciado. 
 # Pra rodar em tempo de execucao vai precisar dar um cleanup e instanciar nova simulation()
@@ -50,7 +61,7 @@ SCREEN_WIDTH:           int = 1920
 SCREEN_HEIGHT:          int = 1080
 SCREEN_WIDTH_MARGIN:    int = 200
 SCREEN_HEIGHT_MARGIN:   int = 100
-NUM_BIRDS:              int = 1000
+NUM_BIRDS:              int = 100
 PLATFORM_SYSTEM: str = platform.system()
 SUPPORTED_PLATFORMS = ["Windows", "Darwin", "Linux"]
 LIBRARY_EXTENSIONS = {
