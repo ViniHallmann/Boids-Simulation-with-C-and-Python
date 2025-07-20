7734e551a4e1b6ef045669dd85735027d34dd33f
vinihallmann <viniciushallmann2015@gmail.com>
2025-07-20 20:44:22 -0300

    att header

--- backend/include/simulation.h
+++ backend/include/simulation.h
@@ -11,6 +11,6 @@ void update_boids(Boids* boids, Grid *grid,
     float centering_factor, float matching_factor, float avoid_factor,
     float turn_factor, float max_speed, float min_speed,
     int screen_width, int screen_height, int margin, int mouse_x, int mouse_y, bool mouse_motion,
-    bool mouse_fear, bool mouse_attraction, int mouse_fear_radius, int mouse_attraction_radius);
+    bool mouse_fear, bool mouse_attraction, int mouse_fear_radius, int mouse_attraction_radius, bool infinite_screen);
 
 #endif
