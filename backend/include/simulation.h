a65ff2753855d1ff45f64308a37291a7968fb0d4
vinihallmann <viniciushallmann2015@gmail.com>
2025-07-20 21:26:13 -0300

    att update_boids header

--- backend/include/simulation.h
+++ backend/include/simulation.h
@@ -6,11 +6,21 @@
 #include <omp.h>
 #include "datatypes.h"
 
-void update_boids(Boids* boids, Grid *grid, 
+typedef enum {
+    BOUNDARY_TURN,   
+    BOUNDARY_BOUNCE, 
+    BOUNDARY_WRAP    
+} BoundaryBehavior;
+
+void update_boids(Boids* boids, Grid *grid, BoundaryBehavior behavior,
     float visual_range, float protected_range,
     float centering_factor, float matching_factor, float avoid_factor,
-    float turn_factor, float max_speed, float min_speed,
-    int screen_width, int screen_height, int margin, int mouse_x, int mouse_y, bool mouse_motion,
-    bool mouse_fear, bool mouse_attraction, int mouse_fear_radius, int mouse_attraction_radius, bool infinite_screen);
+    float turn_factor, float bounce_factor, 
+    float max_speed, float min_speed,
+    int screen_width, int screen_height, int margin,
+    int mouse_x, int mouse_y, bool mouse_motion,
+    bool mouse_fear, bool mouse_attraction,
+    int mouse_fear_radius, int mouse_attraction_radius); 
+
 
 #endif
